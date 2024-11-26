2d3a220643e3c412e750f9d3a9f7c9359d6204e7
mcaiati2 <93667565+mcaiati2@users.noreply.github.com>
2024-11-26 16:27:03 -0500

    commit message

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,6 +6,7 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+//test
 // Serves static files in the entire client's dist folder
 app.use(express.static('../client/dist'));
 app.use(routes);
